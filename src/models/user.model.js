12395c160d84c3b359a624c3411b42321716df21
Kalpesh More <90396306+kalpeshmore1234@users.noreply.github.com>
2024-05-13 11:07:28 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true,
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
